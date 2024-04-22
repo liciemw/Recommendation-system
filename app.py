164c37c97cf09c4e0cdc5f44b47a2e9b23fb627c
Liciemw <liciemumbi@gmail.com>
2024-04-22 11:13:06 +0300

    update app

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 import pickle
 
-app = Flask(__Recommendationsys__)
+app = Flask(__name__)
 
 # Load the trained model
 with open('svd_model.pkl', 'rb') as file:
