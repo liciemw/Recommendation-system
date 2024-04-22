ef4dbbec67b894a2ec46415636b75620f9eb18e3
Liciemw <liciemumbi@gmail.com>
2024-04-22 10:55:23 +0300

    Add app.py

---
+++ Recommendation app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+import pickle
+
+app = Flask(__Recommendationsys__)
+
+# Load the trained model
+with open('svd_model.pkl', 'rb') as file:
+    svd_model = pickle.load(file)
+
+# Define a route for making predictions
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    user_id = data['user_id']
+    movie_id = data['movie_id']
+    
+    # Make predictions using the loaded model
+    prediction = svd_model.predict(user_id, movie_id)
+    
+    # Return the prediction as JSON
+    return jsonify({'user_id': user_id, 'movie_id': movie_id, 'predicted_rating': prediction})
+
+if __name__ == '__main__':
+    app.run(debug=True)
